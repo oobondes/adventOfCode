eeda5e939cb4e3b5a2a0201dc661a915f04768ca
oobndes <oobondes@gmail.com>
2022-01-23 15:50:28 -0500

    Day1 is now solved

--- day1_f.py
+++ day1_f.py
@@ -7,7 +7,7 @@ with open(inp,'r') as file:
 measurements = [int(m) for m in measurements if m]
 
 increases = 0
-for i in range(1,len(measurements)):
-    if measurements[i] > measurements[i-1]: increases += 1
+for i in range(1,len(measurements)-2):
+    if sum(measurements[i:i+3]) > sum(measurements[i-1:i+2]): increases += 1
 
 print(f'The measurement increased {increases} times')
