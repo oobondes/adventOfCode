0ef205bb9de859063c0add2eca18ce9faf6b7b40
oobndes <oobondes@gmail.com>
2022-01-23 23:36:47 -0500

    day 2 is now complete and working

--- runDay.sh
+++ runDay.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
+#default set up if no options are given
 day=1
+pyfile="day${day}.py"
+infile="day$day.txt"
 
+#
 while getopts 'd:ft' flag; do
 	case "$flag" in
-		d) day="${OPTARG}" 
+		d) day="${OPTARG}" #sets which days script to run and to run part 1 with puzzle input if no other arguments are given 
 			pyfile="day${day}.py"
 			infile="day$day.txt";;
-       		f) pyfile="day${day}_f.py" ;;
-   		t) infile="day${day}-test.txt" ;;
+       		f) pyfile="day${day}_f.py" ;; #sets puzzle part 2 to run
+   		t) infile="day${day}-test.txt" ;; #sets test input to be used so the answer can be validated.
 	esac
 done
 
