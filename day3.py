0d2418faca4bb3220c4f8b59a7e11e3410748c45
oobndes <oobondes@gmail.com>
2022-01-24 22:59:46 -0500

    day3 is now complete

---
+++ day3.py
@@ -0,0 +1,33 @@
+import sys
+inp = sys.argv[1]
+
+with open(inp,'r') as file:
+    lines = file.readlines()
+
+lngth = len(lines[0])-1
+halfLngth = len(lines)//2
+tracker = [0 for i in range(lngth)]
+gamma = [0 for i in range(lngth)]
+epsilon = [0 for i in range(lngth)]
+
+for l in lines:
+    for i in range(lngth):
+        if l[i] == '1': 
+            tracker[i] += 1
+print('tracker: ',*tracker)
+for i in range(lngth):
+    if tracker[i] <= halfLngth:
+        gamma[i] = '0'
+        epsilon[i] = '1'
+    else:
+        gamma[i] = '1'
+        epsilon[i] = '0'
+
+print('gamma: ',*gamma,'\n','epsilon: ',*epsilon)
+
+g = int(''.join(gamma),2)
+e = int(''.join(epsilon),2)
+
+print(f'ans: {e*g}')
+
+
---
+++ day3_f.py
@@ -0,0 +1,49 @@
+import sys
+inp = sys.argv[1]
+
+with open(inp,'r') as file:
+    lines = file.readlines()
+
+lngth = len(lines[0])-1
+halfLngth = len(lines)//2
+oxygen = ''
+co2 = ''
+
+for i in range(lngth):
+    oxygen_1 = 0
+    oxygen_0 = 0
+    co2_1 = 0
+    co2_0 = 0
+    ox = list()
+    co = list()
+    for line in lines:
+        if line.startswith(oxygen):
+            ox.append(line)
+            if line[i] == '1':
+                oxygen_1 += 1
+            else:
+                oxygen_0 += 1
+        if line.startswith(co2):
+            co.append(line)
+            if line[i] == '1':
+                co2_1 += 1
+            else:
+                co2_0 += 1
+    if oxygen_1 >= oxygen_0:
+         oxygen += '1'
+    else:
+         oxygen += '0'
+    if co2_1 < co2_0:
+         co2 += '1'
+    else:
+         co2 += '0'
+    if len(ox) == 1:
+        oxygen = ox[0]
+    if len(co) == 1:
+        co2 = co[0]
+
+o = int(oxygen, 2)
+c = int(co2, 2)
+print('oxygen: ',oxygen, ' - ', o)
+print('co2: ', co2, ' - ', c)
+print(f'answer is {c*o}')
